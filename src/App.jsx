bb01a9e24ba17f43774d5e17f04bb334f3cf27c5
Valle <valle@MacBook.local>
2024-10-16 10:20:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import UserList from './components/UserList/UserList';
+
+
+const App = () => {
+return (
+<div>
+<h1>App React con MockAPI.io</h1>
+<UserList/>
+</div>
+);
+};
+
+export default App;
